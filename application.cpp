ca9e5a55f22e748cd461b21c5861f9875d293a05
Zachary Crockett <towynlin@gmail.com>
2016-07-17 18:05:00 -0700

    Makes many PID requests, buffers and publishes

--- application.cpp
+++ application.cpp
@@ -19,25 +19,71 @@ int canMessageCount = 0;
 // OBD CAN message IDs
 const auto OBD_CAN_BROADCAST_ID    = 0X7DF;
 const auto OBD_CAN_REQUEST_ID      = 0x7E0;
-const auto OBD_CAN_REPLY_ID        = 0x7E8;
+const auto OBD_CAN_REPLY_ID_MIN    = 0x7E8;
+const auto OBD_CAN_REPLY_ID_MAX    = 0x7EF;
 
 // OBD services / modes
 const auto OBD_MODE_CURRENT_DATA = 0x01;
 
 // OBD PIDs
-const auto OBD_PID_SUPPORTED_PIDS      = 0x00;
+const auto OBD_PID_SUPPORTED_PIDS_01_20                  = 0x00;
 // MIL = malfunction indicator lamp = check engine light
-const auto OBD_PID_MIL_STATUS          = 0x01;
-const auto OBD_PID_ENGINE_COOLANT_TEMP = 0x05;
-const auto OBD_PID_ENGINE_RPM          = 0x0C;
-const auto OBD_PID_VEHICLE_SPEED       = 0x0D;
-const auto OBD_PID_MAF_SENSOR          = 0x10;
-const auto OBD_PID_O2_VOLTAGE          = 0x14;
-const auto OBD_PID_THROTTLE    	       = 0x11;
+const auto OBD_PID_MIL_STATUS                            = 0x01;
+const auto OBD_PID_FUEL_SYSTEM_STATUS                    = 0x03;
+const auto OBD_PID_ENGINE_LOAD                           = 0x04;
+const auto OBD_PID_COOLANT_TEMPERATURE                   = 0x05;
+const auto OBD_PID_SHORT_TERM_FUEL_TRIM                  = 0x06;
+const auto OBD_PID_LONG_TERM_FUEL_TRIM                   = 0x07;
+const auto OBD_PID_ENGINE_RPM                            = 0x0c;
+const auto OBD_PID_VEHICLE_SPEED                         = 0x0d;
+const auto OBD_PID_TIMING_ADVANCE                        = 0x0e;
+const auto OBD_PID_INTAKE_AIR_TEMPERATURE                = 0x0f;
+const auto OBD_PID_MAF_AIR_FLOW_RATE                     = 0x10;
+const auto OBD_PID_THROTTLE    	                         = 0x11;
+const auto OBD_PID_O2_SENSORS_PRESENT                    = 0x13;
+const auto OBD_PID_O2_SENSOR_2                           = 0x15;
+const auto OBD_PID_OBD_STANDARDS                         = 0x1c;
+const auto OBD_PID_ENGINE_RUN_TIME                       = 0x1f;
+const auto OBD_PID_SUPPORTED_PIDS_21_40                  = 0x20;
+const auto OBD_PID_DISTANCE_TRAVELED_WITH_MIL_ON         = 0x21;
+const auto OBD_PID_COMMANDED_EVAPORATIVE_PURGE           = 0x2e;
+const auto OBD_PID_FUEL_TANK_LEVEL_INPUT                 = 0x2f;
+const auto OBD_PID_WARM_UPS_SINCE_CODES_CLEARED          = 0x30;
+const auto OBD_PID_DISTANCE_TRAVELED_SINCE_CODES_CLEARED = 0x31;
+const auto OBD_PID_ABS_BAROMETRIC_PRESSURE               = 0x33;
+const auto OBD_PID_O2_SENSOR_1                           = 0x34;
+const auto OBD_PID_CATALYST_TEMPERATURE_BANK1_SENSOR1    = 0x3c;
+const auto OBD_PID_SUPPORTED_PIDS_41_60                  = 0x40;
 
-
-const uint8_t pid = OBD_PID_MIL_STATUS;
+const size_t NUM_PIDS_TO_REQUEST = 21;
+const uint8_t pidsToRequest[NUM_PIDS_TO_REQUEST] = {
+	OBD_PID_ENGINE_LOAD,
+	OBD_PID_COOLANT_TEMPERATURE,
+	OBD_PID_SHORT_TERM_FUEL_TRIM,
+	OBD_PID_LONG_TERM_FUEL_TRIM,
+	OBD_PID_ENGINE_RPM,
+	OBD_PID_VEHICLE_SPEED,
+	OBD_PID_TIMING_ADVANCE,
+	OBD_PID_INTAKE_AIR_TEMPERATURE,
+	OBD_PID_MAF_AIR_FLOW_RATE,
+	OBD_PID_THROTTLE,
+	OBD_PID_O2_SENSOR_2,
+	OBD_PID_ENGINE_RUN_TIME,
+	OBD_PID_DISTANCE_TRAVELED_WITH_MIL_ON,
+	OBD_PID_COMMANDED_EVAPORATIVE_PURGE,
+	OBD_PID_FUEL_TANK_LEVEL_INPUT,
+	OBD_PID_WARM_UPS_SINCE_CODES_CLEARED,
+	OBD_PID_DISTANCE_TRAVELED_SINCE_CODES_CLEARED,
+	OBD_PID_ABS_BAROMETRIC_PRESSURE,
+	OBD_PID_O2_SENSOR_1,
+	OBD_PID_CATALYST_TEMPERATURE_BANK1_SENSOR1,
+	OBD_PID_SUPPORTED_PIDS_41_60
+};
+uint8_t pidIndex = NUM_PIDS_TO_REQUEST - 1;
 const int ledPin = D7;
+
+String dumpForPublish;
+int numBufferedMessages = 0;
 
 auto *obdLoopFunction = sendObdRequest;
 unsigned long transitionTime = 0;
@@ -71,12 +117,15 @@ void loop() {
  * and: https://en.wikipedia.org/wiki/OBD-II_PIDs#Standard_PIDs
  */
 void sendObdRequest() {
+	pidIndex = (pidIndex + 1) % NUM_PIDS_TO_REQUEST;
+
     CANMessage message;
     message.id = OBD_CAN_BROADCAST_ID;
     message.len = 8; // just always use 8
     message.data[0] = 0x02; // 0 = single-frame format, 2  = num data bytes
     message.data[1] = OBD_MODE_CURRENT_DATA; // OBD MODE
-    message.data[2] = pid; // OBD PID
+    message.data[2] = pidsToRequest[pidIndex]; // OBD PID
+
     carloop.can().transmit(message);
 
     obdLoopFunction = waitForObdResponse;
@@ -84,7 +133,7 @@ void sendObdRequest() {
 }
 
 void waitForObdResponse() {
-    if(millis() - transitionTime >= 100) {
+    if (millis() - transitionTime >= 100) {
         obdLoopFunction = delayUntilNextRequest;
         transitionTime = millis();
         return;
@@ -93,32 +142,43 @@ void waitForObdResponse() {
     bool responseReceived = false;
 	String dump;
     CANMessage message;
-    while(carloop.can().receive(message)) {
+    while (carloop.can().receive(message)) {
 		canMessageCount++;
-		if(message.id == OBD_CAN_REPLY_ID && message.data[2] == pid) {
-			responseReceived = true;
-		}
 		if (message.id == 0x130) {
 			if (!byteArray8Equal(message.data, lastMessageData)) {
 				memcpy(lastMessageData, message.data, 8);
 				dump += dumpMessage(message);
+				numBufferedMessages++;
 			}
 		} else {
+			if (message.id >= OBD_CAN_REPLY_ID_MIN &&
+					message.id <= OBD_CAN_REPLY_ID_MAX &&
+					message.data[2] == pidsToRequest[pidIndex]) {
+				responseReceived = true;
+			}
 			dump += dumpMessage(message);
+			numBufferedMessages++;
 		}
-    }
-	Serial.write(dump);
+	}
 
-    if(responseReceived) {
+	Serial.write(dump);
+	dumpForPublish += dump;
+	if (numBufferedMessages >= 9) {
+		Particle.publish("m", dumpForPublish, 60, PRIVATE);
+		numBufferedMessages = 0;
+		dumpForPublish.remove(0);
+	}
+
+    if (responseReceived) {
         digitalWrite(ledPin, HIGH);
     }
 }
 
 void delayUntilNextRequest() {
-    if(millis() - transitionTime >= 400) {
+    if (millis() - transitionTime >= 80) {
         obdLoopFunction = sendObdRequest;
         transitionTime = millis();
-    } else if(millis() - transitionTime >= 250) {
+    } else if (millis() - transitionTime >= 20) {
         digitalWrite(ledPin, LOW);
     }
 }
@@ -129,7 +189,7 @@ void delayUntilNextRequest() {
 void printValuesAtInterval() {
 	static const unsigned long interval = 20000;
 	static unsigned long lastDisplay = 0;
-	if(millis() - lastDisplay < interval) {
+	if (millis() - lastDisplay < interval) {
 		return;
 	}
 	lastDisplay = millis();
@@ -143,14 +203,11 @@ void printValues() {
 }
 
 String dumpMessage(const CANMessage &message) {
-	String str = String::format("{\"timestamp\":%f,\"id\":\"0x%03x\",\"data\":\"", millis() / 1000.0, message.id, message.len);
-	for(int i = 0; i < message.len; i++) {
-		if(i == 0) {
-			str += "0x";
-		}
+	String str = String::format("%.2f:", millis() / 1000.0);
+	for (int i = 0; i < message.len; i++) {
 		str += String::format("%02x", message.data[i]);
 	}
-	str += "\"}\n";
+	str += ",";
 	return str;
 }
 
